2a7a247b9a87bf91cb39cefc23ca117712ffed25
vitaxa <vitaxa93gamebox@gmail.com>
2021-11-15 15:17:09 +0300

    CI WIP

--- render.sh
+++ render.sh
@@ -7,6 +7,8 @@ render() {
 sedStr="
   s!%%BASE_IMAGE_NAME%%!$imageName!g;
   s!%%SERVICE_NAME%%!$serviceName!g;
+  s!%%BRANCH%%!$BRANCH!g;
+  s!%%COMMIT_HASH%%!$COMMIT_HASH!g;
 "
 
 sed -r "$sedStr" $1
